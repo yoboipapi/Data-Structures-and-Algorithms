cc3960456b7ef10928bdca014779d33eaf648a92
yoboipapi <arevalogabrieljoshua@gmail.com>
2024-12-06 09:36:49 +0800

    Add files via upload

---
+++ pseudocode.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void prepareFruitSalad() {
+    // Step 1: Boiled a Water in Pot
+    cout << "Heat Half of water in a pot until it boils." << endl;
+
+    // Step 2: Cook noodles
+    cout << "Put the Pancit Canton noodles in the boiling water." << endl;
+    cout << "Let the noodles cook for 15-20 minutes. Stir them so they don't stick." << endl;
+
+    // Step 3: Put a Ladies Choice Mayo
+    cout << "Mix in the Noodles that came with the noodles." << endl;
+
+    // Step 4: Final cooking
+    cout << "Mix all the ingredients, noodles with mayo and mix fruits ." << endl;
+
+    // Step 5: Serve
+    cout << "Put the salad in a bowl and eat!" << endl;
+}
+
+int main() {
+    prepareFruitSalad();
+    return 0;
+}
